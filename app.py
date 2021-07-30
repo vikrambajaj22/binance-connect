02bbd38c7a2a50f18960b8e45a30804831bbb91e
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-07-29 20:24:37 -0400

    add initial api, home page

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask.templating import render_template
+
+from util import connect_to_binance
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/get-balances', methods=['POST'])
+def get_balances():
+    results = {}
+    error = ''
+    if request.method == 'POST':
+        try:
+            connect_to_binance()
+        except Exception as e:
+            error = '{}'.format(repr(e))
+            return render_template('index.html', results=results, error=error)
+    return render_template('index.html', results=results, error=error)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host='0.0.0.0')
