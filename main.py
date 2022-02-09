99451d5bc6e641b90214ef3c5ef291e0c1b73b46
Vikram Bajaj <vikrambajaj220496@gmail.com>
2022-02-08 21:55:52 -0500

    replace cron job with app engine cron.yaml (prevents multiple runs)

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import atexit
-
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask.templating import render_template
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
 from util import connect_to_binance, send_email
 
 app = Flask(__name__)
 load_dotenv()
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/api/notify')
 def notify():
     try:
         results = connect_to_binance.compute_balances()
-
         for ticker in results['assets']:
             if ticker != 'USD':
                 current_price, average_purchase_price = results['assets'][ticker][
@@ -25,22 +26,9 @@ def notify():
                     <p><a href="http://binance-connect-22.ue.r.appspot.com/">Binance-Connect</a></p>
                     <p><a href="https://www.binance.us/en/home">Binance US</a></p>'''.format(
                         ticker, current_price, average_purchase_price))
+        return 'success'
     except Exception as e:
-        print('an exception occurred: {}'.format(repr(e)))
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=notify, trigger="interval",
-                  seconds=3600, next_run_time=datetime.now())
-scheduler.start()
-
-# shut down the scheduler when exiting the app
-atexit.register(lambda: scheduler.shutdown())
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+        return 'an exception occurred: {}'.format(repr(e))
 
 
 @app.route('/api/get-balances', methods=['POST'])
@@ -58,5 +46,4 @@ def get_balances():
 
 
 if __name__ == '__main__':
-    # cron job runs twice if debug=True
-    app.run(debug=False, port=8080, host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0', debug=True)
